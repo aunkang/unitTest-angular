d09de29190dbdaa335c7f580a420d96100c8426d
jarturon <jarturon@orcsoft.co.th>
2019-01-18 12:38:35 +0700

    Add for-test component and a bit testing

--- unit-test-first-time/src/app/app.module.ts
+++ unit-test-first-time/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ForTestComponent } from './component/for-test/for-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForTestComponent
   ],
   imports: [
     BrowserModule
---
+++ unit-test-first-time/src/app/component/for-test/for-test.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-for-test',
+  templateUrl: './for-test.component.html',
+  styleUrls: ['./for-test.component.css']
+})
+export class ForTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  setSomething(val: number): string {
+    let returnValue: string;
+    if (val - this.getRandomValue() < 1) {
+      returnValue = 'Success';
+    } else {
+      returnValue = 'Fail';
+    }
+    return returnValue;
+  }
+  getRandomValue(): number {
+    const a = 10;
+    const c = 20;
+    return (a + c) * 2;
+  }
+
+
+}
